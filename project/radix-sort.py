ad56de90503adace5d9d59366a4db077db061b08
Alex <theaxluo@gmail.com>
2021-05-13 00:15:36 -0500

    finished project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,3 +1,4 @@
+from unittest import TestCase
 import urllib
 import requests
 
@@ -7,4 +8,33 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    arr = book_to_words(book_url)
+    longest = len(max(arr, key=len))
+    
+    for x in range(longest):
+        arr = sort(arr, x, longest - 1)
+    
+    return arr
+
+def sort(book, idx, max):
+    out = [0 for x in range(len(book))]
+    count = [0 for x in range(128)]
+
+    for word in book:
+        if max - idx < len(word):
+            count[word[max - idx]] += 1
+        else:
+            count[0] += 1
+    
+    for x in range(128):
+        count[x] += count[x-1]
+
+    for x in range(len(book) -1, -1, -1):
+        if max - idx < len(book[x]):
+            out[count[book[x][max - idx]] - 1] = book[x]
+            count[book[x][max - idx]] -= 1
+        else:
+            out[count[0] - 1] = book[x]
+            count[0] -= 1
+    
+    return out
